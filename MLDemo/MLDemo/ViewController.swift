d03e671451a7bfae07792ae5a6905aa3967b6426
Subhra Roy <subhra.roy84@yahoo.in>
2019-03-23 17:20:03 +0530

    Core ML Demo

---
+++ MLDemo/MLDemo/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  MLDemo
+//
+//  Created by iSubhra on 23/03/19.
+//  Copyright © 2019 Subhra Roy. All rights reserved.
+//
+
+import UIKit
+import CoreML
+
+class ViewController: UIViewController, UINavigationControllerDelegate {
+
+    @IBOutlet weak var capturedImageView: UIImageView!
+    @IBOutlet weak var predictedLabel: UILabel!
+    
+    private var model: ImageClassifierDemo!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        model = ImageClassifierDemo()
+    }
+
+    @IBAction func didOpenCamera(_ sender: Any) {
+        
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            return
+        }
+        
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .camera
+        cameraPicker.allowsEditing = false
+        
+        present(cameraPicker, animated: true)
+
+        
+    }
+    
+    @IBAction func didOpenLibrary(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+    }
+    
+}
+
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        predictedLabel.text = "Analyzing Image..."
+        
+     //   print("\(info)")
+        
+        guard let image = info[.originalImage] as? UIImage else {
+            return
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return
+        }
+        
+       /* CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) //3
+        
+        context?.translateBy(x: 0, y: newImage.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))*/
+        capturedImageView.image = newImage
+        
+        guard let prediction = try? model.prediction(image: pixelBuffer!) else {
+            return
+        }
+        
+        predictedLabel.text = "I think this is a \(prediction.classLabel)."
+        
+    }
+    
+}
